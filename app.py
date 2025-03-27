d2974a1d36ecb6ddc5dd9f03866adc7cd16a36e1
Shaheen Hyder K <shaheenhyderk@gmail.com>
2025-03-27 21:08:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,12 +23,12 @@ application.add_middleware(
 )
 
 
-@application.head("/ping")
+@application.head("/api/v1/drugfreekerala/ping/")
 async def ping():
     return HTMLResponse(content=None, status_code=status.HTTP_204_NO_CONTENT)
 
 
-@application.post("/drugfreekerala/create", description="Create a new user")
+@application.post("/api/v1/drugfreekerala/create/", description="Create a new user")
 async def create(data: CreateUserDataRequest, session: Session = Depends(get_session)):
     try:
         data = UserData(name=data.name, email=data.email)
@@ -39,7 +39,7 @@ async def create(data: CreateUserDataRequest, session: Session = Depends(get_ses
         return {"message": "User already exists"}
 
 
-@application.get("/drugfreekerala/get", description="Get all users")
+@application.get("/api/v1/drugfreekerala/get/", description="Get all users")
 async def get_all_users(
     session: Session = Depends(get_session), email: EmailStr = Query(...)
 ):
@@ -51,7 +51,7 @@ async def get_all_users(
     }
 
 
-@application.get("/drugfreekerala/total", description="Get total number of users")
+@application.get("/api/v1/drugfreekerala/total/", description="Get total number of users")
 async def get_total_users(session: Session = Depends(get_session)):
     data = session.query(UserData).count()
     return {"total": data}
